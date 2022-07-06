a1d2f21e63ae0824ab15738c9f17dd9099f09c73
sasi12781a <sasi.chimmili@xcubelabs.com>
2022-07-06 13:11:44 +0530

    m

---
+++ App.js
@@ -0,0 +1,47 @@
+import 'react-native-gesture-handler';
+
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+const Drawer=createDrawerNavigator();
+
+const D=()=>{
+  return(
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+    </Drawer.Navigator>
+  )
+}
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator >
+        <Tab.Screen name='home' component={D}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
